91af711c24d31cf28db337eaaa0cdebd7c656008
Elijjah De Castro <edecastro@SVI.COM.PH>
2019-12-04 16:37:44 +0800

    2nd

---
+++ src/app/models/pokedex.ts
@@ -0,0 +1,23 @@
+// export interface Pokedex {
+//   count: number;
+//   next: string;
+//   previous?: any;
+//   results: PokeList[];
+// }
+
+// export interface PokeList {
+//   name: string;
+//   url: string;
+// }
+
+export interface Result {
+  name: string;
+  url: string;
+}
+
+export interface Pokedex {
+  count: number;
+  next: string;
+  previous?: any;
+  results: Result[];
+}
--- src/app/services/pokemon-list.service.ts
+++ src/app/services/pokemon-list.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Pokedex } from '../models/pokedex';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonListService {
-  pokemonListUrl:string = 'https://pokeapi.co/api/v2/pokemon';
+
+  pokemonListUrl: string = 'https://pokeapi.co/api/v2/pokemon';
 
 constructor(private http: HttpClient) { }
+
+
 
 }
